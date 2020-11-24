57e3def4619eae1e7baa41cd1ddc8c7195d56320
MohamadOuf <72459027+MohamadOuf@users.noreply.github.com>
2020-11-24 09:07:02 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
+//echo '<p>this is PHP! </p>';
+
+if (isset($_GET{["FIrstName"])){//show feedback
+echo $_GET{["FIrstName"];
+} else {//show form
+echo'
+<form> "action"
+first Name: <input type="text" name="FirstName" /><br />
+<input type="submit" />
+</form>
 
 
 
+</form>
 
+';
 
-
-
-?>
+}
